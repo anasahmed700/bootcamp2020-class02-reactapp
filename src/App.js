9d6711a1ca604e3796604cbda5cb13bb8363653f
Anas Ahmed <anasahmed700@gmail.com>
2020-06-10 02:06:07 +0500

    Class 2 Functional/stateless components, props in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Stateless from './components/stateless.component.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World from Anas Ahmed.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Stateless newProp='Property 1'/>
+        <hr />
+        <Stateless newProp='Property 2'/>
+
     </div>
   );
 }
