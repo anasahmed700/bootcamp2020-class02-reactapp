5fc9da720140fc67488ddc568ffc1a18b9362339
Anas Ahmed <anasahmed700@gmail.com>
2020-06-15 00:02:15 +0500

    Class 4 change component state using hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import './App.css';
-import Stateless from './components/stateless.component.js';
+import React, {useState} from "react";
+import "./App.css";
+import Stateless from "./components/stateless.component.jsx";
+import {Message} from "./components/counter.component"
 
 function App() {
+  // Using react hook useState to change component's state
+  // [variable, setter]
+  let [count, setCount] = useState(1);
+  let [isdaylit, setDaylit] = useState(false)
+
   return (
     <div className="App">
-        <Stateless newProp='Property 1'/>
-        <hr />
-        <Stateless newProp='Property 2'/>
+      {/* Class 3 functional components & props */}
+      <Stateless newProp="Property 1" />
+      <hr />
+      <Stateless newProp="Property 2" />
 
+      {/* Class 4 change state in functional components using setState hook */}
+      <div className={isdaylit ? 'morning' : 'night'}>
+        <Message counter={count}/>
+        <button onClick={() => setCount(++count)}>Counter</button>
+        <h3>Have a good {isdaylit ? "Morning" : "Night"}</h3>
+        <button onClick={() => setDaylit(!isdaylit)}>Switch daylight</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/counter.component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Message(props) {
+  return <h3>Value of counter is: {props.counter}</h3>;
+}
